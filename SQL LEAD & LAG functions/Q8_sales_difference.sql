82996a8f4aa0ea9c3fab7941e5be828a15d67db4
Mansha Patra <manshapatra12@gmail.com>
2026-04-04 13:15:34 +0530

    practice on lead & lag

--- SQL LEAD & LAG functions/Q8_sales_difference.sql
+++ SQL LEAD & LAG functions/Q8_sales_difference.sql
@@ -2,3 +2,7 @@
 
 -- Question:
 -- Calculate the difference between current sales and previous day sales.
+select
+ sales_date,
+ sales_quantity - LAG(sales_quantity) OVER (order by sales _date) as difference
+from sales_data; 
